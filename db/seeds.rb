5f89ca109fcfcac011a8264a97399f6b641154a0
Cole Challand <ctchalland@gmail.com>
2018-02-07 17:14:39 -0600

    Added users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Record.destroy_all
+Face.destroy_all
+VisualProwess.destroy_all
+Sharingan.destroy_all
+
+User.create!([
+  {user_name: "admin", email: "admin@uchiha.com", password_digest: "$2a$10$BbaXKYNVwSw/eSjFzkAj4eDkO.vWcUNHE5lJYwrnV3/CupKOy35lG", full_name: "Itachi Uchiha", birth_date: "5/19/1993", gender: "Male", membership: false, admin: true},
+  {user_name: "test", email: "test@uchiha.com", password_digest: "$2a$10$BbaXKYNVwSw/eSjFzkAj4eDkO.vWcUNHE5lJYwrnV3/CupKOy35lG", full_name: "Bill Bob", birth_date: "2/1/2018", gender: "Q", membership: false, admin: false}
+])
+
+# Record.create!([
+#   {image_uid: "img/symbol/Itachi_Uchiha.gif" , image_name: "Battle with our Eyes"},
+#   {image_uid: "img/symbol/Itachi_Uchiha.gif" , image_name: "Battle with our Eyes"}
+# ])
+
+# VisualProwess.create!([
+#   {anger: 0.000, contempt: 0.000, disgust: 0.000, fear: 0.000, happiness: 0.000, neutral: 0.000, sadness: 0.000, surprise: 0.000},
+#   {anger: 0.000, contempt: 0.000, disgust: 0.000, fear: 0.000, happiness: 0.000, neutral: 0.000, sadness: 0.000, surprise: 0.000}
+# ])
+
+# Face.create!([
+#   {left_px: 0.0, top_px: 0.0, width_px: 0.0, height_px: 0.0, visual_prowess_id: 1, user_id: 2, record_id: 0, session: 0},
+#   {left_px: 0.0, top_px: 0.0, width_px: 0.0, height_px: 0.0, visual_prowess_id: 2, user_id: 2, record_id: 0, session: 0}
+# ])
